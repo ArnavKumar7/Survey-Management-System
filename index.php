af7780ce123f57682a873f4e4750a3edbcc5133e
ArnavKumar7 <arnav4776@gmail.com>
2022-11-15 16:48:53 +0530

    fix: index.php

--- index.php
+++ index.php
@@ -106,8 +106,6 @@
 
   <!-- Main Footer -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2020 <a href="https://www.sourcecodester.com/">sourcecodester.com</a>.</strong>
-    All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Online Survey System</b>
     </div>
